42a9bec9c3dda7af3b2817cb1f79b4dcbe72f9de
Patrick <patrick.feldtmann@gmail.com>
2022-01-31 08:58:10 +0100

    initias commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <q-list bordered separator>
+      <q-slide-item
+        v-for="(item, index) in list"
+        :key="item.label"
+        @left="onRight(index)"
+      >
+        <template v-slot:left>
+          <q-icon name="done" />
+        </template>
+        <q-item>
+          <q-item-section class="text-center">{{ item.label }}</q-item-section>
+        </q-item>
+      </q-slide-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const list = ref([{ label: "Eis" }, { label: "Tee" }]);
+
+function onRight(d) {
+  list.value.splice(d, 1);
+}
+</script>
