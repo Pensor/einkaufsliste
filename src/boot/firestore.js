16a085bcf804c1ed9bd72f830c9ff1160274f284
Patrick <patrick.feldtmann@gmail.com>
2022-02-01 16:08:02 +0100

    add firestore

---
+++ src/boot/firestore.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// Required for side-effects
+import "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn1jPp3Ilg6enYtv50cpz-3uewzwMpKoI",
+  authDomain: "einkaufsliste-1250b.firebaseapp.com",
+  databaseURL: "https://einkaufsliste-1250b.firebaseio.com",
+  projectId: "einkaufsliste-1250b",
+  storageBucket: "einkaufsliste-1250b.appspot.com",
+  messagingSenderId: "497668696065",
+  appId: "1:497668696065:web:ecdf3c6fa8d8b8fe78b800"
+};
+
+
+export default ({ app }) => {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  app.config.globalProperties.$db = firebase.firestore();
+}
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,21 @@
 <template>
-  <q-page>
+  <q-page class="bg-grey-2 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        v-model="text"
+        class="col"
+        outlined
+        square
+        placeholder="Was brauchst du..."
+        dense
+        bg-color="white"
+        @keyup.enter="add"
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" @click="add" />
+        </template>
+      </q-input>
+    </div>
     <q-list bordered separator>
       <q-slide-item
         v-for="(item, index) in list"
@@ -9,20 +25,46 @@
         <template v-slot:left>
           <q-icon name="done" />
         </template>
-        <q-item>
-          <q-item-section class="text-center">{{ item.label }}</q-item-section>
+        <q-item clickable @click="item.amount++" active>
+          <q-item-section>{{ item.label }}</q-item-section>
+          <q-item-section side>x{{ item.amount }}</q-item-section>
         </q-item>
       </q-slide-item>
     </q-list>
   </q-page>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const list = ref([{ label: "Eis" }, { label: "Tee" }]);
-
-function onRight(d) {
-  list.value.splice(d, 1);
-}
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      list: [
+        { label: "Eis", amount: 1 },
+        { label: "Tee", amount: 1 },
+      ],
+      text: "",
+    };
+  },
+  created() {
+    this.$db.collection("einkaufsliste").onSnapshot((query) => {
+      this.list = query.docs.map((doc) => ({
+        label: doc.data().name,
+        amount: 1,
+      }));
+    });
+  },
+  methods: {
+    onRight(d) {
+      this.list.splice(d, 1);
+    },
+    add() {
+      this.list.push({
+        label: this.text,
+        amount: 1,
+      });
+      this.text = "";
+    },
+  },
+};
 </script>
